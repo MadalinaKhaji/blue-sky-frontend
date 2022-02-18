2db909fb190bfc0e8c6c188eefa3b58f7bcc4ef9
MadalinaKhaji <madalina.khaji10@gmail.com>
2022-02-18 16:21:38 +0200

    Added users, user-add and user-edit components

--- src/app/user-management/user-management.module.ts
+++ src/app/user-management/user-management.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserManagementComponent } from './user-management.component';
+import { UsersComponent } from './users/users.component';
+import { UserAddComponent } from './user-add/user-add.component';
+import { UserEditComponent } from './user-edit/user-edit.component';
 
 @NgModule({
-  declarations: [UserManagementComponent],
+  declarations: [UserManagementComponent, UsersComponent, UserAddComponent, UserEditComponent],
   imports: [
     CommonModule
   ],
