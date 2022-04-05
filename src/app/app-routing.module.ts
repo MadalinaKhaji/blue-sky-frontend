b6a89767a05fca88197e2aa7ccf4468c3e7995ec
MadalinaKhaji <madalina.khaji10@gmail.com>
2022-04-05 15:08:39 +0300

    update to angular 9

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ const routes: Routes = [
   { path: 'dashboard', component: DashboardComponent},
   {
     path: 'user-mgmt',
-    loadChildren: './user-management/user-management.module#UserManagementModule'
+    loadChildren: () => import('./user-management/user-management.module').then(m => m.UserManagementModule)
   },
   { path: '',   redirectTo: '/dashboard', pathMatch: 'full' },
   { path: '**', redirectTo: 'dashboard'}, 
--- src/app/core/services/user-management.service.ts
+++ src/app/core/services/user-management.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs';
 import { User } from '../models/user.model';
+import { catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -12,24 +13,34 @@ export class UserManagementService {
 
   constructor(private httpClient: HttpClient) { }
 
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      console.error('An error occurred:', error.error.message);
+    } else {
+      console.error(`Backend returned code ${error.status}, ` + `body was: ${error.error}`);
+    }
+    
+    return throwError('Something bad happened; please try again later.');
+  };
+
   public getAllUsers(): Observable<User[]> {
-    return this.httpClient.get<User[]>(`${this.apiUrl}/all`);
+    return this.httpClient.get<User[]>(`${this.apiUrl}/all`).pipe(catchError(this.handleError));
   }
 
   public getUser(id): Observable<any> {
-    return this.httpClient.get(`${this.apiUrl}/${id}`);
+    return this.httpClient.get(`${this.apiUrl}/${id}`).pipe(catchError(this.handleError));
   }
 
   public addUser(user): Observable<any> {
-    return this.httpClient.post(this.apiUrl, user);
+    return this.httpClient.post(this.apiUrl, user).pipe(catchError(this.handleError));
   }
 
   public editUser(user): Observable<any> {
-    return this.httpClient.put(this.apiUrl, user);
+    return this.httpClient.put(this.apiUrl, user).pipe(catchError(this.handleError));
   }
 
   public deleteUser(id) {
-    return this.httpClient.delete(this.apiUrl + '/' + id);
+    return this.httpClient.delete(this.apiUrl + '/' + id).pipe(catchError(this.handleError));
   }
 
 }
